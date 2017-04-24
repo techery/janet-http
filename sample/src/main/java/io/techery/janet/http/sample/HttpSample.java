8c8afb08766fc87e845cc9ec57ec3b37bf37bd97
Aleksey Malevaniy <a.malevaniy@gmail.com>
2017-04-24 17:57:02 +0300

    Fix trusting issues with sample ok client

--- sample/src/main/java/io/techery/janet/http/sample/HttpSample.java
+++ sample/src/main/java/io/techery/janet/http/sample/HttpSample.java
@@ -2,27 +2,41 @@ package io.techery.janet.http.sample;
 
 import com.google.gson.Gson;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import io.techery.janet.ActionPipe;
 import io.techery.janet.HttpActionService;
 import io.techery.janet.Janet;
 import io.techery.janet.gson.GsonConverter;
 import io.techery.janet.helper.ActionStateSubscriber;
-import io.techery.janet.okhttp3.OkClient;
-import io.techery.janet.http.sample.action.base.BaseAction;
 import io.techery.janet.http.sample.action.TestProgressAction;
 import io.techery.janet.http.sample.action.UserReposAction;
 import io.techery.janet.http.sample.action.UsersAction;
+import io.techery.janet.http.sample.action.base.BaseAction;
 import io.techery.janet.http.sample.model.User;
 import io.techery.janet.http.sample.util.SampleLoggingService;
+import io.techery.janet.okhttp3.OkClient;
+import okhttp3.OkHttpClient;
 import rx.Observable;
 
 public class HttpSample {
 
-    private static final String API_URL = "https://api.github.com";
+    private static final String API_URL = "http://api.github.com";
 
-    public static void main(String... args) {
+    public static void main(String... args) throws NoSuchAlgorithmException, KeyManagementException {
+        OkClient httpClient = new OkClient(createTrustingOkHttpClient());
+
         Janet janet = new Janet.Builder()
-                .addService(new SampleLoggingService(new HttpActionService(API_URL, new OkClient(), new GsonConverter(new Gson()))))
+                .addService(new SampleLoggingService(new HttpActionService(API_URL, httpClient, new GsonConverter(new Gson()))))
                 .build();
 
         ActionPipe<UsersAction> usersPipe = janet.createPipe(UsersAction.class);
@@ -48,7 +62,7 @@ public class HttpSample {
 
 
         janet = new Janet.Builder()
-                .addService(new SampleLoggingService(new HttpActionService("https://httpbin.org", new OkClient(), new GsonConverter(new Gson()))))
+                .addService(new SampleLoggingService(new HttpActionService("https://httpbin.org", httpClient, new GsonConverter(new Gson()))))
                 .build();
 
         janet.createPipe(TestProgressAction.class)
@@ -57,5 +71,39 @@ public class HttpSample {
                         .onSuccess(action -> System.out.println("request finished " + action))
                         .onProgress((action, progress) -> System.out.println(String.format("progress value:%s", progress))));
 
+    }
+
+    private static OkHttpClient createTrustingOkHttpClient() throws NoSuchAlgorithmException, KeyManagementException {
+        // Create a trust manager that does not validate certificate chains
+        final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                    throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                    throws CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[]{};
+            }
+        }};
+        // Install the all-trusting trust manager
+        final SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        // Create an ssl socket factory with our all-trusting manager
+        final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.sslSocketFactory(sslSocketFactory);
+        builder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+        return builder.build();
     }
 }
